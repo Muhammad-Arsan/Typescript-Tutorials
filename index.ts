16d934b41456bfc6890ac6aaba9132ec632d0d39
Muhammad Arsan <muhammad.arsan@optimusfox.com>
2022-05-18 18:52:20 +0500

    any type

---
+++ index.ts
@@ -0,0 +1,19 @@
+ let age:any = 24          // here age is any type and it doesnt mean that age is now a number type 
+
+ age = "sff"               // so here we can chnage age from number to string 
+ console.log(age)
+
+ age = false
+
+ console.log(age)
+
+ age = {name:"sadsads"}
+ console.log(age)
+
+ let myObject : {name: any, age:any}
+
+ myObject = {name : "ddsa", age: 232}
+ console.log(myObject)
+
+myObject = {name : 132, age: "ewqrqwere"}
+ console.log(myObject)
