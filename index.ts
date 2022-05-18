a51ff18d27bddf7f1c58a300b85c093a0bae19a4
Muhammad Arsan <muhammad.arsan@optimusfox.com>
2022-05-18 15:13:26 +0500

    Basic types

---
+++ index.ts
@@ -0,0 +1,14 @@
+let char = 'arsan'
+let age = 23;
+let isBlackBelt = false
+
+// char = 30  cannot assign/redeclare string variable to number
+char = "Muhammad Arsan"
+console.log(char)
+
+const circ = (diameter : number)=>{
+     return diameter * Math.PI
+}
+
+console.log(circ(8))
+//  cannot assign a parameter of type other than number
