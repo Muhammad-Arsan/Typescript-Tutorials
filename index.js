3d75b2c5c92cd17c148ab6f2eff770da9ad36e1a
Muhammad Arsan <muhammad.arsan@optimusfox.com>
2022-05-18 13:41:32 +0500

    Lecture One

---
+++ index.js
@@ -0,0 +1,8 @@
+var char = "Muhammad Arsan";
+var char1 = 324223;
+console.log(char1);
+console.log("MyName", char);
+var inputs = document.querySelectorAll("input");
+inputs.forEach(function (input) {
+    console.log("Input", input);
+});
---
+++ index.ts
@@ -0,0 +1,9 @@
+const char = "Muhammad Arsan";
+const char1= 324223;
+console.log(char1)
+console.log("MyName", char)
+const inputs = document.querySelectorAll("input")
+
+inputs.forEach(input=>{
+console.log("Input",input)     
+})
