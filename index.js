446687f9b6ef488b974c235081b96efc6b35da14
Muhammad Arsan <muhammad.arsan@optimusfox.com>
2022-05-18 15:19:10 +0500

    basic types

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-var char = 'arsan';
+var char = "arsan";
 var age = 23;
 var isBlackBelt = false;
 // char = 30  cannot assign/redeclare string variable to number
 char = "Muhammad Arsan";
 console.log(char);
 var circ = function (diameter) {
-    return diameter * Math.PI;
+  return diameter * Math.PI;
 };
 console.log(circ(8));
 //  cannot assign a parameter of type other than number
