8efb0dbd51d4986c523347dd1aa1e2b461f7c268
Muhammad Arsan <muhammad.arsan@optimusfox.com>
2022-05-20 10:38:30 +0500

    TUPLES

--- public/app.js
+++ public/app.js
@@ -19,3 +19,11 @@ const docTwo = {
     data: ['ahmed', 'ali', 'arsan']
 };
 console.log(docOne, docTwo);
+// tuples 
+let arr = ['df', 334, false]; // in array we can change the values  regardless thier order
+arr[0] = false;
+arr[1] = 'dsfs';
+arr = [33, false, 'dwf'];
+let tup = ['dsffs', 43, false];
+// tup[0] = 343                                              //In tuple type can't do this beacuase in tuples type  we cant change type order of values
+tup[0] = 'arsan'; // it allows because we can chanage vlaues but not the type we already had defined
--- src/app.ts
+++ src/app.ts
@@ -24,3 +24,16 @@ const docTwo: Resource<string[]> = {
 }
 
 console.log(docOne, docTwo)
+
+
+// tuples 
+
+let arr = ['df', 334, false]        // in array we can change the values  regardless thier order
+
+arr[0] = false
+arr[1] = 'dsfs'
+arr = [33, false, 'dwf']
+
+let tup: [string, number, boolean] = ['dsffs', 43, false]
+// tup[0] = 343                                              //In tuple type can't do this beacuase in tuples type  we cant change type order of values
+tup[0] = 'arsan'           // it allows because we can chanage vlaues but not the type we already had defined
