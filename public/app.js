657d4b88955e03d58a5e57adb534c6ae1ce3cc34
Muhammad Arsan <muhammad.arsan@optimusfox.com>
2022-05-20 10:59:30 +0500

    Tuples

--- public/app.js
+++ public/app.js
@@ -1,24 +1,4 @@
 "use strict";
-// ENUMS                                                     // where we set  variables to be predefined constant
-var ResourceType;
-(function (ResourceType) {
-    ResourceType[ResourceType["BOOK"] = 0] = "BOOK";
-    ResourceType[ResourceType["PHONE"] = 1] = "PHONE";
-    ResourceType[ResourceType["LAPTOP"] = 2] = "LAPTOP";
-    ResourceType[ResourceType["PERSON"] = 3] = "PERSON";
-    ResourceType[ResourceType["BALL"] = 4] = "BALL";
-})(ResourceType || (ResourceType = {}));
-const docOne = {
-    uid: 22,
-    resourceType: ResourceType.PHONE,
-    data: { name: 'bilal' }
-};
-const docTwo = {
-    uid: 33,
-    resourceType: ResourceType.PERSON,
-    data: ['ahmed', 'ali', 'arsan']
-};
-console.log(docOne, docTwo);
 // tuples 
 let arr = ['df', 334, false]; // in array we can change the values  regardless thier order
 arr[0] = false;
--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,5 @@
 
 
-// ENUMS                                                     // where we set  variables to be predefined constant
-
-
-enum ResourceType { BOOK, PHONE, LAPTOP, PERSON, BALL }
-
-interface Resource<T> {
-     uid: number;
-     resourceType: ResourceType,
-     data: T
-}
-
-const docOne: Resource<object> = {
-     uid: 22,
-     resourceType: ResourceType.PHONE,
-     data: { name: 'bilal' }
-}
-
-const docTwo: Resource<string[]> = {
-     uid: 33,
-     resourceType: ResourceType.PERSON,
-     data: ['ahmed', 'ali', 'arsan']
-}
-
-console.log(docOne, docTwo)
-
 
 // tuples 
 
